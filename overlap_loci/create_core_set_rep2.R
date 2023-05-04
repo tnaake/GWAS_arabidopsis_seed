06de2c8007e4f2edc43a2323f2b3e439ff81b901
tnaake <thomasnaake@gmx.de>
2023-05-04 11:26:27 +0200

    adjust thresholds

--- overlap_loci/create_core_set_rep2.R
+++ overlap_loci/create_core_set_rep2.R
@@ -1,7 +1,7 @@
 ## negative mode
 
 ## load trueLociLOD files 
-trueLociLOD_normalized <- read.table( 
+trueLociLOD <- read.table( 
     file = "~/GitHub/GWAS_arabidopsis_seed/gwas_complete_met_all_trueLociLOD_rep12_normalized_neg.txt",
     header = TRUE, sep = "\t", dec = ".", quote = "")
 ##trueLociLOD <- read.delim("../gwas_complete_met_all_trueLociLOD_neg.txt",
@@ -11,23 +11,32 @@ trueLociLOD_normalized <- read.table(
 ## load the relation to rep1, rep2, rep_leaf: there is a link between rep2 and 
 ## rep_feng (column mapping_rep2_rep_feng)
 rel_rep1 <- read.csv("~/GitHub/GWAS_arabidopsis_seed/GWAS_leaf_rep2/rep1_negative_match.csv", sep = "\t")
+rel_rep_feng <- read.csv("~/GitHub/GWAS_arabidopsis_seed/GWAS_leaf_rep2/rep_feng_negative_match.csv", sep = "\t")
 
-## determine the covariance that results in 9008 features
-features <- rel_rep1[rel_rep1$cov > 1.5357, "mapping_rep1_rep2"]
+## determine the covariance that results in 9008 features after the truncation step
+features <- rel_rep1[rel_rep1$cov > 1.3874,  "mapping_rep1_rep2"]
 features_rep1 <- lapply(strsplit(features, split = "/"), "[", 1) |>
     unlist()
 features_rep2 <- lapply(strsplit(features, split = "/"), "[", 2) |>
     unlist()
 
+## what are those features in features_feng?
+features_rep_feng_rep2 <- lapply(strsplit(rel_rep_feng[["mapping_rep2_rep_feng"]], split = "/"), "[", 1) |>
+    unlist()
+features_rep_feng_rep_feng <- lapply(strsplit(rel_rep_feng[["mapping_rep2_rep_feng"]], split = "/"), "[", 2) |>
+    unlist()
+features_rep_feng <- features_rep_feng_rep_feng[features_rep_feng_rep2 %in% features_rep2]
+
+
 ## truncate
-trueLociLOD_normalized_cut <- trueLociLOD_normalized[
-    (trueLociLOD_normalized$met_rep1 %in% features_rep1 &
-         trueLociLOD_normalized$met_rep2 %in% features_rep2) |
-        (is.na(trueLociLOD_normalized$met_rep1) & 
-            is.na(trueLociLOD_normalized$met_rep2)), ]
+trueLociLOD_cut <- trueLociLOD |>
+    dplyr::filter(met_rep1 %in% features_rep1 | is.na(met_rep1)) |>
+    dplyr::filter(met_rep2 %in% features_rep2 | is.na(met_rep2)) |>
+    dplyr::filter(met_leaf_feng %in% features_rep_feng | is.na(met_leaf_feng))
+
 
 ## write to file
-write.table(trueLociLOD_normalized_cut, 
+write.table(trueLociLOD_cut, 
     "~/GitHub/GWAS_arabidopsis_seed/gwas_complete_met_all_trueLociLOD_neg_cut.txt", 
     sep = "\t", quote = FALSE)
 
@@ -43,16 +52,16 @@ trueLociLOD <- read.delim(
 rel_rep1 <- read.csv("~/GitHub/GWAS_arabidopsis_seed/GWAS_leaf_rep2/rep1_positive_match.csv", sep = "\t")
 
 ## determine the covariance that results in 12133 features
-features <- rel_rep1[rel_rep1$cov > 3.168, "mapping_rep1_rep2"]
+features <- rel_rep1[rel_rep1$cov > 2.8474, "mapping_rep1_rep2"]
 features_rep1 <- lapply(strsplit(features, split = "/"), "[", 1) |>
     unlist()
 features_rep2 <- lapply(strsplit(features, split = "/"), "[", 2) |>
     unlist()
 
 ## truncate
-trueLociLOD_cut <- trueLociLOD[
-    trueLociLOD$met_rep1 %in% features_rep1 & 
-        trueLociLOD$met_rep2 %in% features_rep2, ]
+trueLociLOD_cut <- trueLociLOD |>
+    dplyr::filter(met_rep1 %in% features_rep1) |>
+    dplyr::filter(met_rep2 %in% features_rep2)
 
 ## write to file
 write.table(trueLociLOD_cut, 
